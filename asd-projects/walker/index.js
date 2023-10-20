1d155e0dab06b3ee7f2c3d26082fe2de3893093b
Joe BlackmonJr <joeblackmonjr37@gmail.com>
2023-10-20 00:03:29 +0000

    saving walker

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -30,7 +30,7 @@ function runProgram(){
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
-
+$(document).on('keyup', handleKeyUp);
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -40,7 +40,9 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
+    repositionGameItem()
+    wallCollision()
+    redrawGameItem()
 
   }
   
@@ -54,18 +56,50 @@ function runProgram(){
 
     if (event.which === KEY.LEFT) {
       console.log("left pressed");
+      walker.speedX = -5;
     }
 
     else if (event.which === KEY.RIGHT) {
       console.log("right pressed");
+      walker.speedX = 5;
     }
 
     else if (event.which === KEY.UP) {
       console.log("up pressed");
+      walker.speedY = -5;
     }
 
     else if (event.which === KEY.DOWN) {
       console.log("down pressed");
+      walker.speedY = 5;
+    }
+  }
+
+
+
+  function handleKeyUp(event) {
+    if (event.which === KEY.ENTER) {
+      console.log("enter pressed");
+    }
+
+    if (event.which === KEY.LEFT) {
+      console.log("left pressed");
+      walker.speedX = 0;
+    }
+
+    else if (event.which === KEY.RIGHT) {
+      console.log("right pressed");
+      walker.speedX = 0;
+    }
+
+    else if (event.which === KEY.UP) {
+      console.log("up pressed");
+      walker.speedY = 0;
+    }
+
+    else if (event.which === KEY.DOWN) {
+      console.log("down pressed");
+      walker.speedY = 0;
     }
   }
 
@@ -82,7 +116,25 @@ $('#walker').css('left', walker.positionX);
 $('#walker').css('top', walker.positionY);
 }
  
+function wallCollision() {
+  var boardWidth = $('#board').width() - 50
+  var boardHeight = $('#board').height()
+  if(walker.positionX > boardWidth) {
+    walker.speedX = walker.speedX - 5
+  }
 
+  if(walker.positionX < 0){
+    walker.speedX = walker.speedX + 5
+  }
+
+  if(walker.positionY > boardWidth) {
+    walker.speedY = walker.speedY - 5
+  }
+
+  if(walker.positionY < 0){
+    walker.speedY = walker.speedY + 5
+  }
+}
   
   function endGame() {
     // stop the interval timer
